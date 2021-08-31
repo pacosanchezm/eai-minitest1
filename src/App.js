bc90884154762145d3e4018d866d3d3da8459c34
pacosanchezm <paco.sanchezm@gmail.com>
2021-08-31 12:04:46 +0000

    t4

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox and codespaces</h1>
-      <h2>Start editing to see some magic encontrar!</h2>
+      <h2>Start editing to see some magic tacto!</h2>
     </div>
   );
 }
